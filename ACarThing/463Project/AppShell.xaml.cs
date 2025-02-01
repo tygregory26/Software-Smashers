a6531d32cc3f9fa8177e67b93a443dc8f1e207d7
Daisy <Daisy.dpr@gmail.com>
2025-01-31 18:22:06 -0600

    Project Creation

---
+++ ACarThing/463Project/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace _463Project
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ACarThing/463Project/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace _463Project
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
