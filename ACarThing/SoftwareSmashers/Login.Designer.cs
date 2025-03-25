e9a536a00e4d5b88be6c2b9b2d294d59d8486f34
Daisy <Daisy.dpr@gmail.com>
2025-03-25 15:14:10 -0500

    Login Page
    Added buttons, text and textboxes

--- ACarThing/SoftwareSmashers/Login.Designer.cs
+++ ACarThing/SoftwareSmashers/Login.Designer.cs
@@ -30,13 +30,19 @@
         {
             label1 = new Label();
             pictureBox1 = new PictureBox();
+            label2 = new Label();
+            label3 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(668, 346);
+            label1.Location = new Point(909, 172);
             label1.Name = "label1";
             label1.Size = new Size(56, 25);
             label1.TabIndex = 0;
@@ -53,10 +59,67 @@
             pictureBox1.TabIndex = 1;
             pictureBox1.TabStop = false;
             // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(909, 286);
+            label2.Name = "label2";
+            label2.Size = new Size(54, 25);
+            label2.TabIndex = 2;
+            label2.Text = "Email";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(920, 397);
+            label3.Name = "label3";
+            label3.Size = new Size(87, 25);
+            label3.TabIndex = 3;
+            label3.Text = "Password";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(880, 339);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(150, 31);
+            textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(880, 425);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(150, 31);
+            textBox2.TabIndex = 5;
+            textBox2.Visible = false;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(824, 492);
+            button1.Name = "button1";
+            button1.Size = new Size(112, 34);
+            button1.TabIndex = 6;
+            button1.Text = "Register";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(983, 492);
+            button2.Name = "button2";
+            button2.Size = new Size(112, 34);
+            button2.TabIndex = 7;
+            button2.Text = "Login";
+            button2.UseVisualStyleBackColor = true;
+            // 
             // Login
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label3);
+            Controls.Add(label2);
             Controls.Add(pictureBox1);
             Controls.Add(label1);
             Name = "Login";
@@ -70,5 +133,11 @@
 
         private Label label1;
         private PictureBox pictureBox1;
+        private Label label2;
+        private Label label3;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Button button1;
+        private Button button2;
     }
 }
