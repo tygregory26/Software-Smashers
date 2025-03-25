3b2b7300bbbb18de16c3f8de4688382963e5e2ce
Daisy <Daisy.dpr@gmail.com>
2025-03-25 15:21:06 -0500

    made login page "pretty"

--- ACarThing/SoftwareSmashers/Login.Designer.cs
+++ ACarThing/SoftwareSmashers/Login.Designer.cs
@@ -42,9 +42,11 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(909, 172);
+            label1.Font = new Font("Verdana", 20.25F, FontStyle.Bold);
+            label1.ForeColor = Color.FromArgb(37, 30, 56);
+            label1.Location = new Point(1046, 190);
             label1.Name = "label1";
-            label1.Size = new Size(56, 25);
+            label1.Size = new Size(148, 49);
             label1.TabIndex = 0;
             label1.Text = "Login";
             label1.Click += label1_Click;
@@ -52,9 +54,9 @@
             // pictureBox1
             // 
             pictureBox1.Image = Properties.Resources.software_smashers_noText;
-            pictureBox1.Location = new Point(263, 72);
+            pictureBox1.Location = new Point(65, 97);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(313, 268);
+            pictureBox1.Size = new Size(723, 596);
             pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
             pictureBox1.TabIndex = 1;
             pictureBox1.TabStop = false;
@@ -62,58 +64,75 @@
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(909, 286);
+            label2.Font = new Font("Verdana", 10F, FontStyle.Bold);
+            label2.ForeColor = Color.FromArgb(37, 30, 56);
+            label2.Location = new Point(990, 294);
             label2.Name = "label2";
-            label2.Size = new Size(54, 25);
+            label2.Size = new Size(74, 25);
             label2.TabIndex = 2;
             label2.Text = "Email";
+            label2.Click += label2_Click;
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(920, 397);
+            label3.Font = new Font("Verdana", 10F, FontStyle.Bold);
+            label3.ForeColor = Color.FromArgb(37, 30, 56);
+            label3.Location = new Point(990, 402);
             label3.Name = "label3";
-            label3.Size = new Size(87, 25);
+            label3.Size = new Size(122, 25);
             label3.TabIndex = 3;
             label3.Text = "Password";
+            label3.Click += label3_Click;
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(880, 339);
+            textBox1.Location = new Point(990, 354);
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(150, 31);
+            textBox1.Size = new Size(331, 31);
             textBox1.TabIndex = 4;
             // 
             // textBox2
             // 
-            textBox2.Location = new Point(880, 425);
+            textBox2.Location = new Point(990, 446);
             textBox2.Name = "textBox2";
-            textBox2.Size = new Size(150, 31);
+            textBox2.Size = new Size(331, 31);
             textBox2.TabIndex = 5;
             textBox2.Visible = false;
+            textBox2.TextChanged += textBox2_TextChanged;
             // 
             // button1
             // 
-            button1.Location = new Point(824, 492);
+            button1.BackColor = Color.FromArgb(225, 166, 145);
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Verdana", 12F, FontStyle.Bold);
+            button1.ForeColor = Color.FromArgb(37, 30, 56);
+            button1.Location = new Point(969, 524);
             button1.Name = "button1";
-            button1.Size = new Size(112, 34);
+            button1.Size = new Size(174, 79);
             button1.TabIndex = 6;
             button1.Text = "Register";
-            button1.UseVisualStyleBackColor = true;
+            button1.UseVisualStyleBackColor = false;
             // 
             // button2
             // 
-            button2.Location = new Point(983, 492);
+            button2.BackColor = Color.FromArgb(225, 166, 145);
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.Font = new Font("Verdana", 12F, FontStyle.Bold);
+            button2.ForeColor = Color.FromArgb(37, 30, 56);
+            button2.Location = new Point(1172, 524);
             button2.Name = "button2";
-            button2.Size = new Size(112, 34);
+            button2.Size = new Size(174, 79);
             button2.TabIndex = 7;
             button2.Text = "Login";
-            button2.UseVisualStyleBackColor = true;
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
             // 
             // Login
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(227, 225, 232);
             Controls.Add(button2);
             Controls.Add(button1);
             Controls.Add(textBox2);
--- ACarThing/SoftwareSmashers/Login.cs
+++ ACarThing/SoftwareSmashers/Login.cs
@@ -21,5 +21,25 @@ namespace SoftwareSmashers
         {
 
         }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
