c7c86878cf598e8077d4ea9546a0df5a9ffba54d
Ty Gregory <155595382+tygregory26@users.noreply.github.com>
2025-04-19 21:17:03 -0600

    Update dbData.cs

--- ACarThing/SoftwareSmashers/dbData.cs
+++ ACarThing/SoftwareSmashers/dbData.cs
@@ -7,7 +7,6 @@ using System.Text;
 using System.Threading.Tasks;
 using MySql.Data.MySqlClient;
 
-
 namespace SoftwareSmashers
 {
     internal class dbData
@@ -38,7 +37,7 @@ namespace SoftwareSmashers
         {
             try
             {
-                string query = "SELECT * FROM group1-csci463_ACarThing.user;";
+                string query = "SELECT * FROM `group1-csci463_ACarThing`.user;";
                 MySqlDataAdapter sqlData = new MySqlDataAdapter(query, connection);
                 DataTable dTable = new DataTable();
                 sqlData.Fill(dTable);
@@ -56,10 +55,8 @@ namespace SoftwareSmashers
         {
             try
             {
-                string query = "SELECT userID FROM user WHERE email = @Email AND password = @Password LIMIT 1;";
+                string query = "SELECT userID FROM `group1-csci463_ACarThing`.user WHERE email = '" + email + "' AND password = '" + password + "' LIMIT 1;";
                 MySqlCommand command = new MySqlCommand(query, connection);
-                command.Parameters.AddWithValue("@Email", email);
-                command.Parameters.AddWithValue("@Password", password);
 
                 using (MySqlDataReader reader = command.ExecuteReader())
                 {
@@ -78,18 +75,13 @@ namespace SoftwareSmashers
 
         public static Boolean newUser(string firstName, string lastName, string email, string phoneNum, string password)
         // creates a new user in the database, returns true if successful and false if unsuccessful. 
-        // we will also need to add another query for creating settings - this would be whatever we want to be set as default settings
-        // we could also just call settings(userID) within this function.
         {
             try
             {
-                string userQ = "INSERT INTO user (firstName, lastName, email, phoneNumber, password) VALUES (@First, @Last, @Email, @Phone, @Pass);";
-                MySqlCommand command = new MySqlCommand(userQ, connection);
-                command.Parameters.AddWithValue("@First", firstName);
-                command.Parameters.AddWithValue("@Last", lastName);
-                command.Parameters.AddWithValue("@Email", email);
-                command.Parameters.AddWithValue("@Phone", phoneNum);
-                command.Parameters.AddWithValue("@Pass", password);
+                string query = "INSERT INTO `group1-csci463_ACarThing`.user " +
+                               "(firstName, lastName, email, phoneNumber, password) VALUES " +
+                               "('" + firstName + "', '" + lastName + "', '" + email + "', '" + phoneNum + "', '" + password + "');";
+                MySqlCommand command = new MySqlCommand(query, connection);
                 command.ExecuteNonQuery();
                 return true;
             }
@@ -102,18 +94,14 @@ namespace SoftwareSmashers
 
         public static Boolean updateUser(string userID, string firstName, string lastName, string email, string phoneNum, string password)
         // updates existing user, returns true if successful and false if unsuccessful. 
-        // see newUser for formatting
         {
             try
             {
-                string updateQ = "UPDATE user SET firstName = @First, lastName = @Last, email = @Email, phoneNumber = @Phone, password = @Pass WHERE userID = @ID;";
-                MySqlCommand command = new MySqlCommand(updateQ, connection);
-                command.Parameters.AddWithValue("@First", firstName);
-                command.Parameters.AddWithValue("@Last", lastName);
-                command.Parameters.AddWithValue("@Email", email);
-                command.Parameters.AddWithValue("@Phone", phoneNum);
-                command.Parameters.AddWithValue("@Pass", password);
-                command.Parameters.AddWithValue("@ID", userID);
+                string query = "UPDATE `group1-csci463_ACarThing`.user SET " +
+                               "firstName = '" + firstName + "', lastName = '" + lastName + "', email = '" + email + "', " +
+                               "phoneNumber = '" + phoneNum + "', password = '" + password + "' " +
+                               "WHERE userID = " + userID + ";";
+                MySqlCommand command = new MySqlCommand(query, connection);
                 command.ExecuteNonQuery();
                 return true;
             }
@@ -137,14 +125,11 @@ namespace SoftwareSmashers
         {
             try
             {
-                string query = "SELECT * FROM vehicle WHERE ownerID = @UserID;";
+                string query = "SELECT * FROM `group1-csci463_ACarThing`.vehicle WHERE ownerID = " + userID + ";";
                 MySqlCommand command = new MySqlCommand(query, connection);
-                command.Parameters.AddWithValue("@UserID", userID);
-
                 MySqlDataAdapter sqlData = new MySqlDataAdapter(command);
                 DataTable dTable = new DataTable();
                 sqlData.Fill(dTable);
-
                 return dTable;
             }
             catch (MySqlException ex)
@@ -156,33 +141,19 @@ namespace SoftwareSmashers
 
         public static Boolean addVehicle(int userID, string make, string model, string year, string vin, int vehicleType, float battery, float fuel, float oil, float engineTemp, string carName)
         // creating a new vehicle attatched to userID
-        // we may be randomizing battery, fuel, oil, and engine temp based on vehicle type
-        // make sure to add userID and vehicleID to drivers table (if someone is adding a vehicle it should be assumed they are a driver)
-        // see newUser for formatting
         {
             try
             {
-                string query = @"INSERT INTO vehicle (make, model, year, vin, vehicleType, batteryLevel, fuelLevel, oilLevel, engineTemp, ownerID, carName)
-                                 VALUES (@Make, @Model, @Year, @VIN, @Type, @Battery, @Fuel, @Oil, @Temp, @Owner, @CarName);";
-                MySqlCommand command = new MySqlCommand(query, connection);
-                command.Parameters.AddWithValue("@Make", make);
-                command.Parameters.AddWithValue("@Model", model);
-                command.Parameters.AddWithValue("@Year", year);
-                command.Parameters.AddWithValue("@VIN", vin);
-                command.Parameters.AddWithValue("@Type", vehicleType);
-                command.Parameters.AddWithValue("@Battery", battery);
-                command.Parameters.AddWithValue("@Fuel", fuel);
-                command.Parameters.AddWithValue("@Oil", oil);
-                command.Parameters.AddWithValue("@Temp", engineTemp);
-                command.Parameters.AddWithValue("@Owner", userID);
-                command.Parameters.AddWithValue("@CarName", carName);
-                command.ExecuteNonQuery();
+                string query = "INSERT INTO `group1-csci463_ACarThing`.vehicle " +
+                               "(make, model, year, vin, vehicleType, batteryLevel, fuelLevel, oilLevel, engineTemp, ownerID, carName) VALUES " +
+                               "('" + make + "', '" + model + "', '" + year + "', '" + vin + "', " + vehicleType + ", " + battery + ", " + fuel + ", " + oil + ", " + engineTemp + ", " + userID + ", '" + carName + "');";
+                MySqlCommand insertCmd = new MySqlCommand(query, connection);
+                insertCmd.ExecuteNonQuery();
 
-                int insertedVehicleID = (int)new MySqlCommand("SELECT LAST_INSERT_ID();", connection).ExecuteScalar()!;
-                string linkQuery = "INSERT INTO drivers (driverLink, vehicleLink) VALUES (@UserID, @VehicleID);";
-                MySqlCommand linkCmd = new MySqlCommand(linkQuery, connection);
-                linkCmd.Parameters.AddWithValue("@UserID", userID);
-                linkCmd.Parameters.AddWithValue("@VehicleID", insertedVehicleID);
+                int insertedVehicleID = Convert.ToInt32(new MySqlCommand("SELECT LAST_INSERT_ID();", connection).ExecuteScalar());
+
+                string query2 = "INSERT INTO `group1-csci463_ACarThing`.drivers (driverLink, vehicleLink) VALUES (" + userID + ", " + insertedVehicleID + ");";
+                MySqlCommand linkCmd = new MySqlCommand(query2, connection);
                 linkCmd.ExecuteNonQuery();
 
                 return true;
