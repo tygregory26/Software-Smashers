f23e139d385a8dc19c3e717ecaa4abfca4e70a46
Daisy <Daisy.dpr@gmail.com>
2025-04-26 11:53:26 -0500

    worked on newCar and editCar

--- ACarThing/SoftwareSmashers/NewCar.cs
+++ ACarThing/SoftwareSmashers/NewCar.cs
@@ -20,6 +20,7 @@ namespace SoftwareSmashers
 
         int carID;
         int userID;
+        Boolean newCar = false;
 
         public NewCar()
         {
@@ -27,6 +28,8 @@ namespace SoftwareSmashers
         }
         public NewCar(int userID, int carID)
         {
+            // being used as edit car (we already have carID)
+            newCar = false;
             InitializeComponent();
             this.carID = carID;
             txt_NewCar_vehicleName.Text = dbData.getCarName(carID);
@@ -34,19 +37,41 @@ namespace SoftwareSmashers
             txt_NewCar_Model.Text = dbData.getCarModel(carID);
             txt_NewCar_Year.Text = dbData.getCarYear(carID);
             txt_NewCar_VIN.Text = dbData.getCarVin(carID);
-
+        }
+        public NewCar(int userID)
+        {
+            // being used as new car (we don't already have carID)
+            newCar = true;
+            InitializeComponent();
         }
 
         private void btn_NewCar_Cancel_Click(object sender, EventArgs e)
         {
-            ((ACarThing)this.Parent).loadCurrCar(carID);
+            if (newCar)
+            {
+                ((ACarThing)this.Parent).loadCarList();
+            }
+            else
+            {
+                ((ACarThing)this.Parent).loadCurrCar(carID);
+            }
         }
 
         private void btn_NewCar_Save_Click_1(object sender, EventArgs e)
         {
-            dbData.addVehicle(userID, txt_NewCar_Make.Text, txt_NewCar_Model.Text, txt_NewCar_Year.Text, txt_NewCar_VIN.Text, 1, .42f, .34f, .45f, .98f, txt_NewCar_vehicleName.Text);
-            ((ACarThing)this.Parent).loadCurrCar(carID);
+            if (newCar)
+            {
+                dbData.addVehicle(userID, txt_NewCar_Make.Text, txt_NewCar_Model.Text, txt_NewCar_Year.Text, txt_NewCar_VIN.Text, 1, .42f, .34f, .45f, .98f, txt_NewCar_vehicleName.Text);
+                // will need to get vehicleID back from this?
+                ((ACarThing)this.Parent).loadCurrCar(carID);
 
+            }
+            else
+            {
+                //dbData.editVehicle();
+                ((ACarThing)this.Parent).loadCurrCar(carID);
+            }
         }
     }
 }
+
